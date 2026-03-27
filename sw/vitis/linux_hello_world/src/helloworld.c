4ba04fc83185a17e3cad0cd7c0cf781b0164f1e0
Chris Lattman <natgeo13@gmail.com>
2026-03-27 05:50:31 +0000

    Initial commit

---
+++ sw/vitis/linux_hello_world/src/helloworld.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <time.h>
+#include <unistd.h>
+
+int main()
+{
+    struct timespec now, next;
+    int interval_sec = 10;
+
+    clock_gettime(CLOCK_MONOTONIC, &next);
+    next.tv_sec += interval_sec;
+
+    while (1) {
+        clock_gettime(CLOCK_MONOTONIC, &now);
+        if (now.tv_sec > next.tv_sec || (now.tv_sec == next.tv_sec && now.tv_nsec >= next.tv_nsec)) {
+            printf("10 second timer went off\r\n");
+            next.tv_sec += interval_sec;
+        }
+        sleep(1);
+        printf("Hello\r\n");
+        sleep(1);
+        printf("World!\r\n");
+    }
+}
